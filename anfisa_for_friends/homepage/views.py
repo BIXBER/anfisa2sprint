ed174b0de3274e8cdd72fae72e9d7708c73e6bd3
Aleksandr <bixber.official@gmail.com>
2023-05-18 16:28:18 +0300

    Added Q module that get to entry some condition

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,19 @@
+import datetime
+from django.db.models import Q
 from django.shortcuts import render
+
+from ice_cream.models import IceCream, Category, Topping, Wrapper
 
 
 def index(request):
-    template = 'homepage/index.html'
-    return render(request, template)
+    template_name = 'homepage/index.html'
+    ice_cream_list = IceCream.objects.values(
+        'id', 'title', 'description',
+        ).filter(
+            (Q(is_on_main=True) & Q(is_published=True)) |
+            (Q(title__contains='пломбир') & Q(is_on_main=False))
+            ).order_by('title')[1:4]
+    context = {
+        'ice_cream_list': ice_cream_list,
+    }
+    return render(request, template_name, context)
